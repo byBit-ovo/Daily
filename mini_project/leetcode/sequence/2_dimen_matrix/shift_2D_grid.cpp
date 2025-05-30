9b4209e38c3e40d669e4eca646e611beb90981ff
xunyi <3302371508@qq.com>
2025-05-30 15:23:05 +0800

    solution4

--- mini_project/leetcode/sequence/2_dimen_matrix/shift_2D_grid.cpp
+++ mini_project/leetcode/sequence/2_dimen_matrix/shift_2D_grid.cpp
@@ -82,3 +82,31 @@ public:
 
     }
 };
+//optimize the space complexity to O(1)
+class Solution4 {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        m = grid.size(),n=grid[0].size();
+        k%=m*n;
+        reverse(grid,0,m*n-1);
+        reverse(grid,0,k-1);
+        reverse(grid,k,m*n-1);
+        return grid;
+    }
+private:
+    int m,n;
+    pair<int,int> GetPosin2(int pos)
+    {
+        return {pos/n,pos%n};
+    }
+    void reverse(vector<vector<int>>& grid,int s,int e)
+    {
+        for(int i=s,j=e;i<=j;++i,--j)
+        {
+            auto p1 = GetPosin2(i),p2 =GetPosin2(j);
+            int x1=p1.first,y1=p1.second,x2=p2.first,y2=p2.second;
+            swap(grid[x1][y1],grid[x2][y2]);
+        }
+    }
+
+};
