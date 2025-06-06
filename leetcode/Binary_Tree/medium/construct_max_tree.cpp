f2ce42928822525e0d31485a4dadab19085d3777
xunyi <3302371508@qq.com>
2025-06-06 16:46:55 +0800

    tree

---
+++ leetcode/Binary_Tree/medium/construct_max_tree.cpp
@@ -0,0 +1,38 @@
+tion for a binary tree node.
+ * struct TreeNode {
+     *  *     int val;
+     *   *     TreeNode *left;
+     *    *     TreeNode *right;
+     *     *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     *      *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     *       *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+     *        * };
+*         */
+class Solution {
+    public:
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+            return build(nums,0,nums.size()-1);
+     }
+        
+     TreeNode* build(vector<int>& nums,int start,int end){
+        if(start > end){
+             return nullptr;
+        }
+     TreeNode* root = nullptr;
+        int rootVal = nums[start];
+        int index = start;
+        for(int i=start;i<=end;++i){
+            if(rootVal<nums[i]){
+             rootVal = nums[i];
+             index = i;
+         }
+     }
+                                        
+     root = new TreeNode(rootVal);
+     root->left = build(nums,start,index-1);
+     root->right = build(nums,index+1,end);
+      return root;
+                                                                        
+                                                                        
+ }
+};
