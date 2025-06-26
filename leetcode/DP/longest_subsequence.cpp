14ef47cae4dca300bb06d0edd7fa9ea4947cab6d
byBit <3302371508@qq.com>
2025-06-26 08:54:59 +0800

    another way to solve

--- leetcode/DP/longest_subsequence.cpp
+++ leetcode/DP/longest_subsequence.cpp
@@ -23,3 +23,26 @@ public:
         return cache[i][j];
     }
 };
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int len1=text1.size(),len2=text2.size();
+        vector<vector<int>> dp(len1+1,vector<int>(len2+1,0));
+        for(int i=len1-1;i>=0;--i)
+        {
+            for(int j=len2-1;j>=0;--j)
+            {
+                if(text1[i]==text2[j])
+                {
+                    dp[i][j] = 1+dp[i+1][j+1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i][j+1],dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
