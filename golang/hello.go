6024715a5f37f22c6b3dfc31eecc14503cd8d5e2
qzr <3302371508@qq.com>
2025-10-08 15:30:51 +0800

    go

--- golang/hello.go
+++ golang/hello.go
@@ -4,6 +4,25 @@ import "fmt"
 func add(a,b int) int{
 	return a+b
 }
+func concat(s1 string, s2 string) string{
+	return s1 + s2
+}
+
+func yearsUntilEvents(age int) (int,int,int){
+	a := 18 - age
+	if a < 0{
+		a = 0
+	}
+	b := 20 - age
+	if b < 0{
+		b = 0
+	}
+	c := 25 - age
+	if c < 0{
+		c = 0
+	}
+	return a, b, c
+}
 func main() {
 	// var str string 
 	// var a int8 
@@ -19,8 +38,15 @@ func main() {
 	const place = "BeiJing"
 	msg := fmt.Sprintf("Hello,%s,You are in %s\n",name,place)
 	fmt.Printf(msg)
-	a := 1
-	if a > 0{
+	
+	if a := 1 ; a < 0{
 		fmt.Printf("greater than 0\n")
+	}else{
+		fmt.Printf("%d\n",a)
 	}
+
+	fmt.Printf(concat("former, ","latter\n"))
+	x,y,z := yearsUntilEvents(20)
+	fmt.Printf("%d,%d,%d\n",x,y,z)
+
 }
