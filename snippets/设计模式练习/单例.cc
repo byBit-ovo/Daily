de2362e5ddbd7b2d2b5195fbda8ecb24ff86ab68
byBit-ovo <q3302371508@gmail.com>
2026-04-02 22:49:42 +0800

    dp

--- snippets/设计模式练习/单例.cc
+++ snippets/设计模式练习/单例.cc
@@ -1,51 +1,34 @@
-#include <iostream>
-#include <limits.h>
-#include <optional>
-#include <queue>
-#include <unordered_map>
 
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
 
-int dfs(std::vector<std::vector<int>>& grid, std::vector<std::vector<int>>& visited,
-	int x, int y) {
-	int n = grid.size(), m = grid[0].size();
-    if(x < 0 || x >= n || y < 0 || y >= m || visited[x][y]) {
-		return -1;
-	}
-	if(grid[x][y] == 1) {
-		return 0;
-	}
-	visited[x][y] = true;
-	int res = -1;
-	for(int k = 0;k < 4;k++) {
-		int nx = x + dx[k], ny = y + dy[k];
-		res = std::max(res, dfs(grid, visited, nx, ny));
-	}
-	visited[x][y] = false;
-	return res + 1;
-}
-// 1 0 0 
-// 1 0 1 
-// 1 0 0 
-// 0 1 0 
 
-class st {
+
+class Singleton {
 public:
-	static st& get(){
-		static st s;
-		return s;
+	static Singleton& get_instance() {
+		static Singleton obj;
+		return obj;
 	}
+	Singleton(const Singleton &s) = delete;
+	Singleton& operator=(const Singleton &s) = delete;
+	// 移动构造需要修改源对象的状态，所以参数不能加const
+	Singleton(Singleton&&) = delete;
+	Singleton& operator=(Singleton&&) = delete;
+
 private:
-	st() = default;
-	st(const st& s) = delete;
-	st& operator=(const st& s) = delete;
+	// 饿汉模式
+	// static Singleton obj;
+	Singleton() = default;
+	~Singleton() = default;
+};
+
+class Singleton_ptr{
+
 };
 
 
 
 int main() {
-	st& a = st::get();
-	st& b = st::get();
-    return 0;
+	Singleton &a = Singleton::get_instance();
+	Singleton &b = Singleton::get_instance();
+	return 0;
 }
